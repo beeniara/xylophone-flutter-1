ccb2889754edb4c64f2864f99963639205429058
Arema <mtteoiaki@gmail.com>
2020-05-12 20:38:03 +1200

    AEIOU

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,63 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  //create sound Method
+
+  void Playsound(){
+    final player = AudioCache();
+    player.play('aeiou1.mp3');
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: <Widget>[
+              FlatButton(
+                color: Colors.red,
+                onPressed: () {
+
+                },
+
+              ),
+
+              FlatButton(
+                color: Colors.yellow,
+                onPressed: () {
+                  Playsound();
+                },
+
+              ),
+
+              FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  Playsound();
+                },
+
+              ),
+              FlatButton(
+                color: Colors.blue,
+                onPressed: () {
+                  Playsound();
+                },
+
+              ),
+              FlatButton(
+                color: Colors.orange,
+                onPressed: () {
+                  Playsound();
+                },
+
+              ),
+            ],
+          ),
         ),
       ),
     );
